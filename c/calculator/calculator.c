5994426278798f52ccdd6eab71095fe7d5341a78
Null <noelle@noellebeck.me>
2025-10-15 21:35:06 -0700

    added calculator.c and compiled c for linux

--- c/calculator/calculator.c
+++ c/calculator/calculator.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int calc(float x, float y, const char o[]);
 
 int main() {
-    float x, y;
-    char o;
-
+    float x = 0;
+    float y = 0;
+    char o[2] = "";
+    printf("Calculator !\n");
     
+    printf("What would you like the first number to be?\n");
+    scanf("%f", &x);
 
-    return 0
+    printf("What about the operation?\n");
+    scanf("%s", o);
+
+    printf("Finally, the second number?\n");
+    scanf("%f", &y);
+
+    calc(x, y, o);
+
+    return 0;
+}
+
+int calc(float x, float y, const char o[]) {
+    if (strcmp(o, "+") == 0) {
+        printf("%f + %f = %f\n", x, y, x + y);
+    } else if (strcmp(o, "-") == 0) {
+        printf("%f - %f = %f\n", x, y, x - y);
+    } else if (strcmp(o, "*") == 0) {
+        printf("%f * %f = %f\n", x, y, x * y);
+    } else if (strcmp(o, "/") == 0) {
+        printf("%f / %f = %f\n", x, y, x / y);
+    } else if (strcmp(o, "^") == 0) {
+        printf("%f ^ %f = %f\n", x, y, powf(x, y));
+    } else {
+        printf("ERR: Invalid operation!\n");
+    }
+
+    return 0;
 }
